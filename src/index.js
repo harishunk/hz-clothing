ccf49adf31890060341f494d407ba9c907ea4ba0
hariszaheer <harishunk1633@gmail.com>
2021-11-03 16:45:20 +0500

    intsalled react-redux and redux-logger, added code for first reducer i.e. userReducer

--- src/index.js
+++ src/index.js
@@ -5,13 +5,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import {Provider} from 'react-redux'
 
 ReactDOM.render(
-  <BrowserRouter>
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-  </BrowserRouter>,
+  <Provider>
+    <BrowserRouter>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
 
