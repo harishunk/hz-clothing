05f2a3b18a39b1acf4b5aceff333ef8205f95199
hariszaheer <harishunk1633@gmail.com>
2021-11-07 04:08:20 +0500

    implemented cart item reducer, add_item  cart action, action definition, property for cartReducer state and add to cart functionality

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -3,14 +3,16 @@ import CollectionItem from '../collection-item/collection-item.component';
 
 import './collection-preview.component.scss'
 
-const CollectionPreview = ({title, items}) => (
+const CollectionPreview = ({ title, items }) => (
     <div className="collection-preview">
         <h1 className="title">{title}</h1>
         <div className="preview">
-            {items.filter((item,idx)=>idx < 4).map(({id, ...otherItemProps}) => (
-                <CollectionItem key={id} {...otherItemProps}/>
-            )
-            )}
+            {items
+                .filter((item, idx) => idx < 4)
+                .map((item) => (
+                    <CollectionItem key={item.id} item={item} />
+                )
+                )}
         </div>
     </div>
 );
