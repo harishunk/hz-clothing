e60e9247cf4f4e640a5376710048300211c9c650
hariszaheer <harishunk1633@gmail.com>
2021-11-16 01:55:23 +0500

    added redux-persist to maintain cart item on reload or sign in and implemented persistor by persistStore and persistReducer

--- src/index.js
+++ src/index.js
@@ -3,19 +3,22 @@ import ReactDOM from 'react-dom';
 
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
-import {Provider} from 'react-redux'
+import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react';
 
-import store from './redux/store'
+import { store, persistor } from './redux/store';
+import App from './App';
 
 
 import './index.css';
-import App from './App';
 
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
       <React.StrictMode>
-        <App />
+        <PersistGate persistor={persistor}>
+          <App />
+        </PersistGate>
       </React.StrictMode>
     </BrowserRouter>
   </Provider>,
